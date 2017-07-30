462fb4ee0e4ab2810c744b5d81e196d76effc08a
Benedikt Schmied <development@benedikt-schmied.de>
2017-07-30 23:54:07 +0200

    last commit for tonight

--- pro_virtual_controller_bus_BERKELEY_SOCKETS/queue.c
+++ pro_virtual_controller_bus_BERKELEY_SOCKETS/queue.c
@@ -17,6 +17,40 @@
 
 /* threading component */
 #include <pthread.h>
+#include <semaphore.h>
+
+/* structures, unions & typedefs */
+
+/*!
+  \brief item
+ */
+struct item__attr {
+	struct item__attr *next;
+	int sd;
+	int len;
+	char *payload;
+};
+
+/*!
+  \brief list (anchor)
+ */
+struct list__attr {
+	pthread_mutex_t mtx;
+	struct item__attr *item;
+};
+
+/*!
+  \brief queue attributes
+ */
+struct queue__attr {
+	struct list__attr *list;
+	sem_t sem;
+};
+
+/* static variables */
+static struct queue__attr *s_queue;
+
+/* API functions */
 
 /*!
   \brief opens a queue
@@ -26,9 +60,32 @@
 int queue__open(void)
 {
 	/* automatic variables */
+	int ret;
 
 	/* executable statements */
 
+	/* first, allocate memory required for the queuing structure */
+	s_queue = (struct queue_attr *)malloc(sizeof(struct queue_attr));
+	if (s_queue) {
+
+		/* automatic variables */
+		pthread_mutexattr_t mattr;
+
+		/* executable statements */
+
+		/* initialize the queue */
+		s_queue->list = (struct list__attr *)malloc(sizeof(struct list__attr));
+
+		/* initialize the attributes for the mutex (part of the list) */
+		pthread_mutexattr_init(&mattr);
+
+		/* initialize the mutex */
+		pthread_mutex_init(&s_queue->list->mtx, &mattr);
+
+		/* now, initialize the semaphore, which is shared between
+		 	 the writing and reading contexts */
+		sem_init(&s_queue->sem, 0, 0);
+	}
 	return 0;
 }
 
@@ -55,8 +112,59 @@ int queue__close(void)
 int queue__add_to(int _sd, char *_p, int _len)
 {
 	/* automatic variables */
+	struct item__attr *item;
+	int ret;
 
 	/* executable statements */
+
+	/* check the functions' arguments */
+	if (_p == NULL || _len < 0) {
+		return -1;
+	}
+
+	/* allocate the memory */
+	item = (struct item__attr *)malloc(sizeof(struct imte__attr));
+
+	/* check whether we've received enough memory */
+	if (item) {
+
+		/* the last item points to itself */
+		item->next = item;
+		item->payload = malloc(_len);
+
+		/* check, whether we've got enough */
+		if (item->payload) {
+			memcpy(item->payload, _p, _len);
+		}
+	}
+
+	/* protect working on the list */
+	ret = pthread_mutex_lock(&s_queue->list->mtx);
+
+	/* check whether we can go on */
+	if (ret == 0) {
+
+		/* automatic variables */
+		struct item__attr *iter;
+
+		/* executable statements */
+		iter = s_queue->list->item;
+
+		/* search for the end of the list */
+		while (iter && iter != iter->next) {
+
+			/* move the stack variable to the next item */
+			iter = iter->next;
+		} /* end of while - loop - statement */
+
+		/* we've reached the end */
+		iter->next = item;
+	}
+
+	pthread_mutex_unlock(&s_queue->list->mtx);
+
+
+	sem_post(&s_queue->sem);
 	return 0;
 }
 
@@ -72,5 +180,16 @@ int queue__remove_from(int _sd, char **_p, int *_len)
 	/* automatic variables */
 
 	/* executable statements */
+
+	/* wait on the writer context to give us a message */
+	sem_wait(&s_queue->sem);
+
+	pthread_mutex_lock(&s_queue->list->mtx);
+
+	/* take the first item's address, and take
+	 	 the second item's address to mark the start of
+	 	 the list */
+
+	pthread_mutex_unlock(&s_queue->list->mtx);
 	return 0;
 }
---
+++ test_virtual_controller_bus_clients/main.c
@@ -0,0 +1,66 @@
+// client.c
+
+/* standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+/* threading library */
+#include <pthread.h>
+
+/* projects' header files */
+#include "unified_sockets.h"
+
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	/* automatic variables */
+    int sd, ret;
+
+    /* executable statements */
+
+    sd = unified_sockets__open();
+    if (sd) {
+
+    	/* automatic variables */
+
+    	/* executable statements */
+    	ret = unified_sockest__connect(sd);
+    	if (ret >= 0) {
+
+    		/* automatic variables */
+			unsigned cnt;
+			int len;
+
+			/* executable statements */
+			printf("Client sending 'hello server!' to server\n");
+			char buf[1024];
+			memset(buf, 0x00, sizeof(buf));
+			strncpy(buf, "test!\0", 20);
+			len = strlen(buf);
+			snprintf(buf + len, sizeof(buf) - len, " instance %s", argv[1]);
+			printf("%s\n", buf);
+
+			for (cnt = 0; cnt < 10; cnt++) {
+
+				/* automatic variables */
+
+				/* executable statements */
+
+				printf("write returns %i\n", unified_sockets__send(sd, buf, strlen(buf)));
+				memset(buf, 0x00, sizeof(buf));
+				int count = unified_sockest__recv(sd, buf, 1000);
+				printf("I'm %s, Got echo of %s from server\n", argv[1], buf);
+			}
+    	}
+    }
+	shutdown(sd, SD_BOTH);
+}
