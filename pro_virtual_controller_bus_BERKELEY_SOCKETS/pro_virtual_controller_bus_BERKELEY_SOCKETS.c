37cb6d18e619c32a3d590f53e9bde85bbc046d95
Benedikt Schmied <development@benedikt-schmied.de>
2017-08-02 23:57:50 +0200

    creating threads in production code

--- pro_virtual_controller_bus_BERKELEY_SOCKETS/pro_virtual_controller_bus_BERKELEY_SOCKETS.c
+++ pro_virtual_controller_bus_BERKELEY_SOCKETS/pro_virtual_controller_bus_BERKELEY_SOCKETS.c
@@ -19,7 +19,12 @@ void error(const char *msg)
     exit(1);
 }
 
-void* echo(void* param)
+/*!
+  \brief treas incoming message
+  \param []     void
+  \return    socket descriptor [>=0] if successful
+ */
+void* treat_incoming_message(void* param)
 {
     /* automatic variables */
     int s = (int)param;
@@ -37,9 +42,8 @@ void* echo(void* param)
 
     while (count >= 0) {
 
-        printf("Server received %s\n", buf);
-        printf("Server sending it back\n");
-        unified_sockets__send(s, buf, strlen(buf));
+        /* push the message into the buffer */
+        queue__add_to(s, buf, count);
 
         count = unified_sockets__recv(s, buf, 1000);
     }
@@ -90,22 +94,23 @@ int echo_sync(int _fd)
  */
 void *broadcasting_messages(void *_p) {
 
-    /* loop forever */
-    while (1) {
+    /* automatic variables */
+    int ret, icnt, sd;
+    char *p;
+    int len;
 
-        /* automatic variables */
-        char *p; /* pointer to an incoming message */
-        int len, sd; /* length of message */
+    /* executable statements */
 
-        /* executable statements */
-        queue__remove_from(&sd, &p, &len);
+    ret = queue__remove_from(&sd, &p, &len);
+    while (ret >= 0) {
 
-        /* set the the first descriptor */
+        /*  */
 
 
-
-    } /* end of while - loop - statement */
-
+        /* before checking the loop statement again, we have to call
+             the remove from function */
+        ret = queue__remove_from(&sd, &p, &len);
+    }
     return NULL;
 }
 
@@ -127,6 +132,9 @@ int main(int argc, char *argv[])
 
     /* executable statements */
     printf("starting server\n");
+
+    /* we need to startup the queue */
+    queue__open();
 
     /* initialize the attributes */
     pthread_attr_init(&tattr);
@@ -159,7 +167,7 @@ int main(int argc, char *argv[])
 
             /* creates a thread which takes care of the newly accepted
                  socket */
-            pthread_create(&t, 0, echo, (void*)newsd);
+            pthread_create(&t, 0, treat_incoming_message, (void*)newsd);
 
             /* wait for the next connection to pop up */
             newsd = unified_sockets__accept(sd);
